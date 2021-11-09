d8ea09d22bb6b7ede7304e5842ac98a9621e91f0
RemyChazelas <remychazelas@gmail.com>
2021-11-09 14:19:56 +0100

    hope last add

--- routes/auth.js
+++ routes/auth.js
@@ -3,22 +3,22 @@ const Auth = require("../models/auth");
 const { hashPassword, verifyPassword } = require("../service/Argon2");
 const { createToken } = require("../service/Jwt");
 
-authRouter.post("/signin", async (req, res) => {
-    const { pseudo, likes, email, password } = req.body;
-    try {
-        const user = await Auth.findByEmail(email);
-        if (user) {
-            throw new Error("DUPLICATA");
-        }
-        const hashedPassword = await hashPassword(password);
-        await Auth.create(pseudo, likes, email, hashedPassword);
-        return res.status(201).json("Utilisateur créé");
-    } catch (error) {
-        if (error.message === "DUPLICATA")
-            return res.status(401).json("L'Email existe deja");
-        return res.status(500).json(error);
-    }
-});
+// authRouter.post("/signin", async (req, res) => {
+//     const { pseudo, likes, email, password } = req.body;
+//     try {
+//         const user = await Auth.findByEmail(email);
+//         if (user) {
+//             throw new Error("DUPLICATA");
+//         }
+//         const hashedPassword = await hashPassword(password);
+//         await Auth.create(pseudo, likes, email, hashedPassword);
+//         return res.status(201).json("Utilisateur créé");
+//     } catch (error) {
+//         if (error.message === "DUPLICATA")
+//             return res.status(401).json("L'Email existe deja");
+//         return res.status(500).json(error);
+//     }
+// });
 
 authRouter.post("/login", async (req, res) => {
     const { email, password } = req.body;
@@ -38,8 +38,7 @@ authRouter.post("/login", async (req, res) => {
         if (error.message === "WRONG_CREDENTIALS") {
             return res.status(401).json("Email et Password ne correspondent pas");
         }
-
-        return res.status(500).json("Error server");
+        return res.status(500).json("Error server login");
     }
 });
 
