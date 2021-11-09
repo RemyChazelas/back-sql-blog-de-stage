b6a7e1d838d8008d82e4a1b8a077a365b4580dbb
RemyChazelas <remychazelas@gmail.com>
2021-11-09 12:03:25 +0100

    second test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const dotenv = require("dotenv");
 const app = express();
 const connection = require("./config/db")
@@ -15,7 +15,7 @@ connection.connect((err) => {
 })
 
 dotenv.config();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
